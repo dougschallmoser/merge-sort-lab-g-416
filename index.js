e01c94660c415b2a9157760ca6252cdbc0db6ac2
Doug Schallmoser <dougschallmoser@gmail.com>
2020-12-07 02:54:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,5 +17,7 @@ function merge(firstHalf, secondHalf) {
 
 function mergeSort(array) {
   let midpoint = array.length / 2 
-  let firstHalf = array.slice()
+  let firstHalf = array.slice(0, midpoint)
+  let secondHalf = array.slice(midpoint, array.length)
+  
 }
